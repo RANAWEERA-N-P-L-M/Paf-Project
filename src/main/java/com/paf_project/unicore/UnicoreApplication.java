22a74375ab68c1df8d606452339386e0be7bd694
Lasal Ranaweera <it23554436@my.sliit.lk>
2026-04-11 13:00:54 +0530

    fix: update scanBasePackages to include both com.unicore and com.paf_project.unicore

--- src/main/java/com/paf_project/unicore/UnicoreApplication.java
+++ src/main/java/com/paf_project/unicore/UnicoreApplication.java
@@ -3,7 +3,7 @@ package com.paf_project.unicore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.unicore")
+@SpringBootApplication(scanBasePackages = {"com.unicore", "com.paf_project.unicore"})
 public class UnicoreApplication {
 
 	public static void main(String[] args) {
