03fba859dedc6ea401ad57a8df6baacf264e6d08
ShaknaRizath <shakna48@gmail.com>
2026-04-18 11:26:42 +0530

    add bookingService for managing booking operations

---
+++ frontend/src/services/bookingService.js
@@ -0,0 +1,31 @@
+import axios from 'axios'
+import { API_BASE } from '../config/api'
+import authService from './authService'
+
+const API_URL = `${API_BASE}/bookings`
+
+const bookingService = {
+  createBooking: (data) => axios.post(API_URL, data, authService.getAuthHeader()),
+
+  getMyBookings: () => axios.get(`${API_URL}/my`, authService.getAuthHeader()),
+
+  getAllBookings: () => axios.get(API_URL, authService.getAuthHeader()),
+
+  approveBooking: (id, message) =>
+    axios.put(
+      `${API_URL}/${id}/approve`,
+      { adminResponse: message ?? '' },
+      authService.getAuthHeader()
+    ),
+
+  rejectBooking: (id, reason) =>
+    axios.put(
+      `${API_URL}/${id}/reject`,
+      { adminResponse: reason },
+      authService.getAuthHeader()
+    ),
+
+  cancelBooking: (id) => axios.put(`${API_URL}/${id}/cancel`, {}, authService.getAuthHeader()),
+}
+
+export default bookingService
